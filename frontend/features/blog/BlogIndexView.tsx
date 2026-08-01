e037321c9c013a3848216b5554b70a4d7b7cef37
yahayakenny <y.hkehinde@yahoo.com>
2026-08-01 09:04:38 +0100

    updates and bug fixes

--- frontend/features/blog/BlogIndexView.tsx
+++ frontend/features/blog/BlogIndexView.tsx
@@ -64,7 +64,7 @@ export function BlogIndexView() {
           lottie="searchNoResults"
         />
       ) : (
-        <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-12">
+        <ul className="blog-index-grid">
           {posts.map((post) => (
             <li key={post.id} className="min-w-0">
               <article className="h-full">
